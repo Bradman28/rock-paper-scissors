15d6fc35deec466fec2cd3776204d2de095a5ff1
Bradman28 <bsurinak28@gmail.com>
2022-06-15 22:37:22 -0500

    Created working function for gameplay

--- script.js
+++ script.js
@@ -2,18 +2,45 @@
 - computer generates random 'rock', 'paper' 'scissor'
 - user inputs their selection
 - determine what beats what, if user select rock etc console.log "You win"
-- message to determine result of round */
+- message to determine result of round
+- first to 5 wins the game */
 
 //function that generates random choice for computer
 function computerPlay () {
-    let computerRandom = [
+    let randomOptions = [
         "Rock",
         "Paper",
         "Scissors",
     ];
-    let randomChoice = computerRandom[Math.floor(Math.random()*computerRandom.length)];
-    console.log(randomChoice);
-    return randomChoice;
+    let computerRandom = randomOptions[Math.floor(Math.random()*randomOptions.length)];
+    console.log(computerRandom);
+    return computerRandom;
 }
 
-const computerTurn = computerPlay()
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "Paper") {
+        console.log ("You Lose! Paper beats Rock!")
+    }
+    else if (playerSelection === "rock" && computerSelection === "Scissors") {
+        console.log ("You Win! Rock beats Scissors!")
+    }
+    else if (playerSelection === "paper" && computerSelection === "Scissors") {
+        console.log ("You Lose! Scissors beat Paper!")
+    }
+    else if (playerSelection === "paper" && computerSelection === "Rock") {
+        console.log ("You Win! Paper beats Rock!")
+    }
+    else if (playerSelection === "scissors" && computerSelection === "Rock") {
+        console.log ("You Lose! Rock beats Scissors!")
+    }
+    else if (playerSelection === "scissors" && computerSelection === "Paper") {
+        console.log ("You Win! Scissors beat Paper!")
+    }
+    else {
+        console.log ("Tie! No Winners for this round.")
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
