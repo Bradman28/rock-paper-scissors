e9ca270d7b56511033116c84a69a741b66dcf918
Bradman28 <bsurinak28@gmail.com>
2022-07-19 23:24:01 -0500

    created for loop to console scores each round, with five round limit and game over message

--- script.js
+++ script.js
@@ -5,42 +5,86 @@
 - message to determine result of round
 - first to 5 wins the game */
 
+let playerScore = 0
+let computerScore = 0
+
 //function that generates random choice for computer
+
 function computerPlay () {
     let randomOptions = [
-        "Rock",
-        "Paper",
-        "Scissors",
+        "rock",
+        "paper",
+        "scissors",
     ];
     let computerRandom = randomOptions[Math.floor(Math.random()*randomOptions.length)];
     console.log(computerRandom);
     return computerRandom;
 }
 
+// function that gives input for player
+
+function playerPlay () {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    console.log (input)
+    return input;
+}
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "Paper") {
-        console.log ("You Lose! Paper beats Rock!")
-    }
-    else if (playerSelection === "rock" && computerSelection === "Scissors") {
-        console.log ("You Win! Rock beats Scissors!")
-    }
-    else if (playerSelection === "paper" && computerSelection === "Scissors") {
-        console.log ("You Lose! Scissors beat Paper!")
-    }
-    else if (playerSelection === "paper" && computerSelection === "Rock") {
-        console.log ("You Win! Paper beats Rock!")
-    }
-    else if (playerSelection === "scissors" && computerSelection === "Rock") {
-        console.log ("You Lose! Rock beats Scissors!")
-    }
-    else if (playerSelection === "scissors" && computerSelection === "Paper") {
-        console.log ("You Win! Scissors beat Paper!")
-    }
-    else {
-        console.log ("Tie! No Winners for this round.")
+        if (playerSelection == "rock" && computerSelection == "paper") {
+            computerScore++;
+            console.log ("You Lose! Paper beats Rock!")
+        }
+        else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore++;
+            console.log ("You Win! Rock beats Scissors!")
+        }
+        else if (playerSelection === "paper" && computerSelection === "scissors") {
+            computerScore++;
+            console.log ("You Lose! Scissors beat Paper!")
+        }
+        else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerScore++;
+            console.log ("You Win! Paper beats Rock!")
+        }
+        else if (playerSelection === "scissors" && computerSelection === "rock") {
+            computerScore++;
+            console.log ("You Lose! Rock beats Scissors!")
+        }
+        else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerScore++;
+            console.log ("You Win! Scissors beat Paper!")
+        }
+        else if (playerSelection === computerSelection) {
+            console.log ("Tie! No Winners for this round.")
+        }
+        else {
+            console.log ("No good")
+        }
+}
+
+const playerSelection = playerPlay();
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+
+function game() {
+    for (let i = 0; i <= 5; i++) {
+        playRound();
+        if (i<5) {
+            console.log ("My score" + " " + playerScore)
+            console.log ("Computer score" + " " + computerScore)
+
+        }
+        else if (i >=5) {
+            console.log ("Game Over")
+        }
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+game ()
+
+
